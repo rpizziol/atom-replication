023cdb958c82b886740c084d272e937e1881a978
emilio <emilio@mpbwalt.local>
2021-12-12 20:47:53 +0100

    2task validation

--- LQN-CRN/2task.py
+++ LQN-CRN/2task.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     Tclient=[]
     e=[]
     
-    for i in range(3):
+    for i in range(15):
     
         X0[-1] = np.random.randint(low=10,high=100)
         MU[4] = 1
