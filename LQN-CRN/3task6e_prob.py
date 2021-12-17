d16e83d49d0c814caef1413601bbd80ab5ecda59
emilio <emilio@mpbwalt.local>
2021-12-17 10:00:30 +0100

    fixed ?

--- LQN-CRN/3task6e_prob.py
+++ LQN-CRN/3task6e_prob.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
     Tclient = []
     e = []
     
-    for i in range(30):
+    for i in range(1):
     
         X0[-1] = np.random.randint(low=10, high=500)
         
