821d7377fff3fc207ea9945ca647064233691265
rpizziol <roberto.pizziol@gmail.com>
2022-01-21 14:33:38 +0100

    Commenting and lqsim implementation

--- genetic/main.m
+++ genetic/main.m
@@ -2,15 +2,26 @@ clear
 close('all');
 rng('default'); % For replication of the experiment 
 
+%% Model definition
+% Model with 3 tasks
 sourcemodel = './res/model1.lqn';
 temppath = './out/temp1.lqn';
-
-%sourcemodel = './res/model2.lqn';
-%temppath = './out/temp2.lqn';
-
 model.N = 3; % Number of microservices (tasks)
 model.M = 3; % Number of classes (max entries)
+% Starting service time for each entry
+st = [0.0012, 0.0021, 0.0037, 0.0051];
+nuser = 3000;
 
+% Complete model
+%sourcemodel = './res/model2.lqn';
+%temppath = './out/temp2.lqn';
+%model.N = 7; % Number of microservices (tasks)
+%model.M = 3; % Number of classes (max entries)
+% Starting service time for each entry
+%st = [0.0012, 0.0021, 0.0037, 0.0051, 0.0022, 0.0019, 0.0048, 0.0174, 0.0056];
+%nuser = 3000;
+
+%% Objective function's parameters
 %params.psi = rand(model.N, model.M); % Weights of transactions
 params.psi = zeros(model.N, model.M); % Weights of transactions
 params.psi(1,1) = 1;
@@ -22,43 +33,26 @@ params.psi(1,1) = 1;
 params.tau1 = 0.5; % Objective function weight 1
 params.tau2 = 0.5; % Objective function weight 2
 
-% Constraints for r and s
-constraints.Q = [1200, 1200]; %randi([2, 10000], 1, model.N-1); % Max number of replicas for each microservice
+%% Constraints for r and s
+constraints.Q = [1200, 1200]; % Max number of replicas for each microservice
 % CPU share for each replica of each microservice for the time interval t
-constraints.s_lb = [0.001, 0.001]; %randi([0, 200], 1, model.N-1) / 1000; % Lower bound (0 - 0.2)
-constraints.s_ub = [100, 100]; %randi([800, 100000], 1, model.N-1) / 1000; % Upper bound (0.8 - 100.0)
-
-% Arrays defined for functions testing
-rv = [1000, 1000];
-
-st = [0.0012, 0.0021, 0.0037, 0.0051]; % Starting service time for each entry
-
-%cpushare = [0.5, 1];
-
-%[np2, st2] = calculateByCPUShare(st, cpushare);
-
-% Generate a correct lqn file
-%updateModel(sourcemodel, temppath, 'rv', rv);
-%updateModel(temppath, temppath, 'st', st2);
-%updateModel(temppath, temppath, 'np', np2);
-
-updateModel(sourcemodel, temppath, 'nuser', 3000);
-
-
+constraints.s_lb = [0.001, 0.001];  % Lower bound
+constraints.s_ub = [100, 100];      % Upper bound
 Cmax = sum(constraints.s_ub); %constraints.Q.*constraints.s_ub);
 
-% fval = fitness(cpushare, sourcemodel, st, rv, model, params, Cmax);
-
+%% Genetic algorithm
+updateModel(sourcemodel, temppath, 'nuser', nuser);
 tic()
-f = @(x)fitness(x, sourcemodel, st, rv, model, params, Cmax);
 
+f = @(x)fitness(x, sourcemodel, st, rv, model, params, Cmax);
 options = optimoptions('ga'); % Load default settings
 options = optimoptions(options,'PopulationType', 'doubleVector');
-options = optimoptions(options,'PopulationSize', 50); % 50 is default
-options = optimoptions(options,'MaxGenerations', 200); % 100*nvars is default (200)
+options = optimoptions(options,'PopulationSize', 50); % default: 50
+options = optimoptions(options,'MaxGenerations', 200); % default: 100*nvars
 options = optimoptions(options,'MaxStallGenerations', 50);
-options = optimoptions(options,'MutationFcn', {  @mutationadaptfeasible 0.1 });
+options = optimoptions(options,'MutationFcn', { @mutationadaptfeasible 0.1 });
 options = optimoptions(options,'PlotFcn', {@gaplotbestf, @gaplotbestindiv }); %@printState
 options = optimoptions(options,'Display', 'iter');
-[x, fval, exitflag, output, population, scores] = ga(f, model.N -1, [], [], [], [], constraints.s_lb, constraints.s_ub, [], [], options);
+[x, fval, exitflag, output, population, scores] = ga(f, model.N -1, [],...
+    [], [], [], constraints.s_lb, constraints.s_ub, [], [], options);
 toc()
