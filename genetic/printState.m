7397124aae871e5d8b40659cd0eeda72cb92c2a4
rpizziol <roberto.pizziol@gmail.com>
2022-01-27 17:15:39 +0100

    Saving all best cpu shares by generation

--- genetic/printState.m
+++ genetic/printState.m
@@ -1,15 +1,18 @@
 function state = printState(options, state, flag) %[state,options,optchanged] 
-    %global allBest
-    %allBest = [allBest; state.Best];
-    disp('Best');
-    disp(state.Best);
-    disp('Population');
-    disp(state.Population);
-    disp('Selection');
-    disp(state.Selection);
-    disp('Expectation');
-    disp(state.Expectation);
-    disp('Score');
-    disp(state.Score);
+    global allBest
+    
+%     disp('Best');
+%     disp(state.Best);
+%     disp('Population');
+%     disp(state.Population);
+%     disp('Score');
+%     disp(state.Score);
+
+    % Find the index of the 'Score' equal to 'Best'
+    index = find(state.Score == state.Best);
+    % Use that index to select the member of the population who had that
+    bestIndividual = state.Population(index, :);
+    disp(bestIndividual);
+    allBest = [allBest; bestIndividual];
 end
 
