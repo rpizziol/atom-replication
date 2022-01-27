64e43cd25ec2ae512f4c486ec4fd3a9c20949b92
rpizziol <roberto.pizziol@gmail.com>
2022-01-27 16:29:57 +0100

    Try to save cpu share by iteration 2

--- genetic/printState.m
+++ genetic/printState.m
@@ -1,7 +1,8 @@
 function state = printState(options, state, flag) %[state,options,optchanged] 
     global allBest
     allBest = [allBest; state.Best];
-    %disp(state.Best);
-    disp(state.Population(state.Selection));
+    disp(state.Best);
+    disp(state.Population);
+    disp(state.Selection);
 end
 
