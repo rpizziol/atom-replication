fb821fff28f79a1849677b831b8650d49ae3f98e
rpizziol <roberto.pizziol@gmail.com>
2022-01-27 16:59:49 +0100

    More display during ga

--- genetic/printState.m
+++ genetic/printState.m
@@ -1,9 +1,15 @@
 function state = printState(options, state, flag) %[state,options,optchanged] 
     %global allBest
     %allBest = [allBest; state.Best];
+    disp('Best');
     disp(state.Best);
+    disp('Population');
     disp(state.Population);
+    disp('Selection');
     disp(state.Selection);
+    disp('Expectation');
     disp(state.Expectation);
+    disp('Score');
+    disp(state.Score);
 end
 
