3d05d3507fad6d3edabc100550ebd0f0debe8126
rpizziol <roberto.pizziol@gmail.com>
2022-01-27 17:59:28 +0100

    Bug fix

--- genetic/printState.m
+++ genetic/printState.m
@@ -9,12 +9,14 @@ function state = printState(options, state, flag) %[state,options,optchanged]
 %     disp(state.Score);
 
     % Find the index of the 'Score' equal to 'Best'
-    index = find(state.Score == state.Best(end));
-%     [~, p] = ismember(state.Score, state.Best, 'rows');
-%     index = find(p, 1);
-    % Use that index to select the member of the population who had that
-    bestIndividual = state.Population(index, :);
-    disp(bestIndividual);
-    allBest = [allBest; bestIndividual];
+    if(size(state.Best) >= 1)
+        index = find(state.Score == state.Best(end));
+    %     [~, p] = ismember(state.Score, state.Best, 'rows');
+    %     index = find(p, 1);
+        % Use that index to select the member of the population who had that
+        bestIndividual = state.Population(index, :);
+        disp(bestIndividual);
+        allBest = [allBest; bestIndividual];
+    end
 end
 
