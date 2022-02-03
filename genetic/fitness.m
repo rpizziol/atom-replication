bd112256c14151869c2150d5d27d548a63060b4b
rpizziol <roberto.pizziol@gmail.com>
2022-02-03 17:16:58 +0100

    new constraint attempt

--- genetic/fitness.m
+++ genetic/fitness.m
@@ -1,6 +1,10 @@
 function value = fitness(cpushare, sourcemodel, st, rv, model, params, Cmax, nuser)
     newModelName = 'fittmp';
     
+    % Best response times possible (calculated with lqsim)
+    rt_best = [0.00568904, 0.00210005, 0.00732427, 0.0165358, 0.00378144, ...
+    0.00348144, 0.00710729, 0.0197073, 0.00790729];
+
     %       temppath = './out/fittmp.lqn';
     temppath = './out/fittmp.lqnx';
     updateModel(sourcemodel, temppath, 'nuser', nuser);
@@ -14,11 +18,11 @@ function value = fitness(cpushare, sourcemodel, st, rv, model, params, Cmax, nus
     % Obtain response time (service-time from xml)
     [value, rt] = solveModel(newModelName, model, params, Cmax, rv, cpushare);
     %% Check if st2 violates the SLA
-%    % SLA is 110% of the nominal service time
-%     SLA = st*1.1;
-%     if sum(rt > SLA) > 0 % At least one value violates the SLA
-%         value = 10;
-%     else
+    % SLA is 110% of the nominal service time
+    SLA = rt_best*1.1;
+    if sum(rt > SLA) > 0 % At least one value violates the SLA
+        value = 10;
+    else
         value = -value; % This is just for optimtool (minimize)
-%     end
+    end
 end
