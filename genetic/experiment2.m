0ba5ed05d3a9ba8d317aa099684acf2787a117b7
rpizziol <roberto.pizziol@gmail.com>
2022-02-11 16:12:03 +0100

    Experiment 2 bug fix

--- genetic/experiment2.m
+++ genetic/experiment2.m
@@ -43,7 +43,9 @@ for j = 1:3
     global bestValues
     global bestIndividuals
     global bestTimeStamps
-    %global bestThroughputs
+    global nusersInTime
+    global timeSlots
+    global times
     
     %% Genetic algorithm
     global start 
@@ -56,7 +58,7 @@ for j = 1:3
     options = optimoptions(options,'PopulationType', 'doubleVector');
     options = optimoptions(options,'PopulationSize', 50); % default: 50
     options = optimoptions(options,'MaxGenerations', 400); % default: 100*nvars
-    options = optimoptions(options,'MaxTime', 600);
+    options = optimoptions(options,'MaxTime', 300);
     options = optimoptions(options,'MaxStallGenerations', 10);
     options = optimoptions(options,'MutationFcn', { @mutationadaptfeasible 0.1 });
     options = optimoptions(options,'PlotFcn', {@gaplotbestf, @gaplotbestindiv, @printState });
