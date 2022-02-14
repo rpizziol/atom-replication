0bd99d70f8d16d24c135498700e3a7e1a7a70d19
rpizziol <roberto.pizziol@gmail.com>
2022-02-14 15:19:35 +0100

    bug fix

--- genetic/readNUser.m
+++ genetic/readNUser.m
@@ -1,6 +1,6 @@
 function nuser = readNUser()
     xdoc = xmlread('./res/atom-full_template6.lqnx');
     tasks = xdoc.getElementsByTagName('task');
-    nuser = tasks.item(0).getAttribute('multiplicity');
+    nuser = floor(str2double(tasks.item(0).getAttribute('multiplicity')));
 end
 
