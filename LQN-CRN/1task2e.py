f3970ebb14c1a73a206a22042cff023b00469910
emilio-imt <emilio-imt@MacBook-Pro-di-emilio-imt.local>
2022-10-20 14:53:34 +0200

    running exmampl

--- LQN-CRN/1task2e.py
+++ LQN-CRN/1task2e.py
@@ -5,8 +5,8 @@ Created on 8 dic 2021
 '''
 
 from entity import *
-from validation import lqnsValidator
-from validation import matlabValidator
+#from validation import lqnsValidator
+#from validation import matlabValidator
 import matplotlib.pyplot as plt
 import numpy as np
         
@@ -14,75 +14,82 @@ if __name__ == '__main__':
     
     # task declaration
     cTask = Task(name="Client", ref=True)
-    Router = Task(name="Router")
+    ms = Task(name="MS")
     
     # entries declaration
-    Addr1 = Entry("Address1")
-    Addr2 = Entry("Address2")
+    search = Entry("Search")
     browse = Entry("Browse")
     
     cTask.addEntry(browse)
-    Router.addEntry(Addr1)
-    Router.addEntry(Addr2)
+    ms.addEntry(search)
     
     # activity declaration
     
-    # Router  entries logic#
-    Addr1.getActivities().append(Activity(stime=1.0, parent=Addr1, name="e"))
-    Addr2.getActivities().append(Activity(stime=1.0, parent=Addr2, name="e"))
+    # Search  entries logic#
+    choiceSearch=probChoice(parent=search, name="Choice")
+    BlkC1=actBlock(parent=choiceSearch, name="BlkC1")
+    BlkC1.activities.append(Activity(stime=1.0, parent=BlkC1, name="c1"))
+    BlkC2=actBlock(parent=choiceSearch, name="BlkC2")
+    BlkC2.activities.append(Activity(stime=1.0, parent=BlkC2, name="c2"))
+    
+    choiceSearch.addBlock(BlkC1, "P_c1")
+    choiceSearch.addBlock(BlkC2, "P_c2")
+    
+    search.getActivities().append(choiceSearch)
+    search.getActivities().append(Activity(stime=1.0, parent=search, name="e"))
+    
     
     # client logic#
-    browse.getActivities().append(SynchCall(dest=Addr1, parent=browse, name="2Address1"))
-    browse.getActivities().append(SynchCall(dest=Addr2, parent=browse, name="2Address2"))
+    browse.getActivities().append(SynchCall(dest=search, parent=browse, name="2searc"))
     browse.getActivities().append(Activity(stime=1.0, parent=browse, name="browse"))
     
     lqn2crn = LQN_CRN2()
-    lqn2crn.getCrn({"task":[cTask, Router], "name":"1task2e"})
+    lqn2crn.getCrn({"task":[cTask,ms], "name":"runExp"})
     
     lqn2crn.toMatlab(outDir="../model/validation")
     
-    # validate the model against lqns#
-    matV = matlabValidator("../model/validation/1task2e/lqn.m")
-    lqnV = lqnsValidator("../model/validation/1task2e/lqn_t.lqn")
-    
-    X0 = [0 for i in range(7)]
-    MU = [0 for i in range(7)]
-    NC = [0 for i in range(2)]
-    NT = [0 for i in range(2)]
-    rep = 1
-    dt = 10.0 ** -1
-    TF = 3000 * dt
-    
-    T_mat=[]
-    Tclient=[]
-    e=[]
-    
-    for i in range(5):
-    
-        X0[-1] = np.random.randint(low=10,high=300)
-        MU[2] = 1
-        MU[5] = 1
-        MU[6] = 1
-        NC[1] = np.random.randint(low=10,high=300)
-        NT[1] = np.random.randint(low=10,high=300)
-    
-        print(X0[-1],NC[1:],NT[1:])
-    
-        T_mat.append(matV.solveModel(X0, MU, NT, NC,dt))
-        T_lqns = lqnV.solveModel(X0=X0[-1], NT=NT, NC=NC)
-        for t in T_lqns:
-            if(t["task"]=="Client"):
-                Tclient.append(t["trg"])
-                break
-    
-        e.append(abs(T_mat[-1]-Tclient[-1])*100/Tclient[-1])
-    
-    plt.figure()
-    plt.stem(Tclient,linefmt="b", markerfmt="bo",label="lqns")
-    plt.stem(T_mat,linefmt="g", markerfmt="go",label="matlb")
-    plt.legend()
-    plt.savefig("1task2e_validation.pdf")
-    
-    print(e)
-    print(Tclient)
-    print(T_mat)
+    # # validate the model against lqns#
+    # matV = matlabValidator("../model/validation/1task2e/lqn.m")
+    # lqnV = lqnsValidator("../model/validation/1task2e/lqn_t.lqn")
+    #
+    # X0 = [0 for i in range(7)]
+    # MU = [0 for i in range(7)]
+    # NC = [0 for i in range(2)]
+    # NT = [0 for i in range(2)]
+    # rep = 1
+    # dt = 10.0 ** -1
+    # TF = 3000 * dt
+    #
+    # T_mat=[]
+    # Tclient=[]
+    # e=[]
+    #
+    # for i in range(5):
+    #
+    #     X0[-1] = np.random.randint(low=10,high=300)
+    #     MU[2] = 1
+    #     MU[5] = 1
+    #     MU[6] = 1
+    #     NC[1] = np.random.randint(low=10,high=300)
+    #     NT[1] = np.random.randint(low=10,high=300)
+    #
+    #     print(X0[-1],NC[1:],NT[1:])
+    #
+    #     T_mat.append(matV.solveModel(X0, MU, NT, NC,dt))
+    #     T_lqns = lqnV.solveModel(X0=X0[-1], NT=NT, NC=NC)
+    #     for t in T_lqns:
+    #         if(t["task"]=="Client"):
+    #             Tclient.append(t["trg"])
+    #             break
+    #
+    #     e.append(abs(T_mat[-1]-Tclient[-1])*100/Tclient[-1])
+    #
+    # plt.figure()
+    # plt.stem(Tclient,linefmt="b", markerfmt="bo",label="lqns")
+    # plt.stem(T_mat,linefmt="g", markerfmt="go",label="matlb")
+    # plt.legend()
+    # plt.savefig("1task2e_validation.pdf")
+    #
+    # print(e)
+    # print(Tclient)
+    # print(T_mat)
