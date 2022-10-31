bb4a9fd28532d6faf8cd528a7870184090f086a0
rpizziol <roberto.pizziol@gmail.com>
2022-10-31 14:16:43 +0100

    New experiment 1000-o

--- genetic/runAllATOMExperiments.m
+++ genetic/runAllATOMExperiments.m
@@ -4,14 +4,14 @@
 % runExperimentByMix('browsing', [0.63, 0.32, 0.05], 2000);
 % clear
 % runExperimentByMix('browsing', [0.63, 0.32, 0.05], 3000);
-clear
-runExperimentByMix('shopping', [0.54, 0.26, 0.20], 1000);
+% clear
+% runExperimentByMix('shopping', [0.54, 0.26, 0.20], 1000);
 % clear
 % runExperimentByMix('shopping', [0.54, 0.26, 0.20], 2000);
 % clear
 % runExperimentByMix('shopping', [0.54, 0.26, 0.20], 3000);
-% clear
-% runExperimentByMix('ordering', [0.33, 0.17, 0.50], 1000);
+clear
+runExperimentByMix('ordering', [0.33, 0.17, 0.50], 1000);
 % clear
 % runExperimentByMix('ordering', [0.33, 0.17, 0.50], 2000);
 % clear
