a09975e3f2fe3111119814e99b023a6cddb1ce97
rpizziol <roberto.pizziol@gmail.com>
2022-11-16 15:47:40 +0100

    Tp matrix fill

--- ACMEAIR/main.m
+++ ACMEAIR/main.m
@@ -4,6 +4,14 @@ sourcefile = 'acmeair.lqn';
 tempfile = 'acmeair-2.lqn';
 outfile = 'acmeair-2.lqxo';
 
+Tp = zeros(size(wi.Tm));
+RTp = zeros(size(wi.RTm));
+
+entries = ["clientEntry", "MSauthEntry", "MSvalidateidEntry", ...
+    "MSviewprofileEntry", "MSupdateprofileEntry", "MSupdateMilesEntry", ...
+    "MSbookflightsEntry", "MScancelbookingEntry", "MSqueryflightsEntry", ...
+    "MSgetrewardmilesEntry"];
+
 % Load data and what if
 data = load('./acmeAir.py_full_data_out.mat');
 wi = load('./acmeAir.py_full_10b.mat');
@@ -13,10 +21,12 @@ for i = 1:25
     updateModel(sourcefile, tempfile, 'W', [wi.Cli(i)]);
     updateModel(tempfile, tempfile, 'nc', wi.NC(i,:));
     % Solve the model
-    [status, ~] = system("lqns -x " + tempfile, '-echo');
+    [status, ~] = system("lqns -x " + tempfile);
     % Obtain output throughput and service time
     if status == 0
-        disp(getAttributeByEntry(outfile, 'clientEntry', 'throughput'));
+        for j = 1:size(entries, 2)
+            Tp(i, j) = getAttributeByEntry(outfile, entries(i), 'throughput');
+        end
         delete(outfile);
     end
 end
