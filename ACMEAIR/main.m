9f4487e2223ad6f0b655c4b52363db8f0026abdc
rpizziol <roberto.pizziol@gmail.com>
2022-11-16 14:47:56 +0100

    First validation template

--- ACMEAIR/main.m
+++ ACMEAIR/main.m
@@ -1,5 +1,20 @@
 clear
-load('/home/robb/MEGA/IMT/Research/Autoscalers/ACMEAIR/nodejsMicro/data/acmeAir.py_full_data_out.mat')
 
-updateModel('acmeair.lqn', 'acmeair-2.lqn', 'W', [Cli(end)]);
-updateModel('acmeair-2.lqn', 'acmeair-2.lqn', 'nc', NCopt(end,:));
+sourcefile = 'acmeair.lqn';
+tempfile = 'acmeair-2.lqn';
+outfile = 'acmeair-2.lqxo';
+
+% Load data and what if
+data = load('./acmeAir.py_full_data_out.mat');
+wi = load('./acmeAir.py_full_10b.mat');
+
+for i = 1:25
+    % Update model with number of users and cores
+    updateModel(sourcefile, tempfile, 'W', [wi.Cli(i)]);
+    updateModel(tempfile, tempfile, 'nc', wi.NC(i,:));
+    % Solve the model
+    [status, ~] = system("lqns -x" + tempfile);
+    % Obtain output throughput and service time
+    disp(getAttributeByEntry(outfile, 'clientEntry', 'throughput'));
+end
+
