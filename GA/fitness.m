3086f868f88391b9ed004ce1d0ce14f5ddbc59d9
rpizziol <roberto.pizziol@gmail.com>
2022-12-19 19:46:13 +0100

    Bug fix

--- GA/fitness.m
+++ GA/fitness.m
@@ -1,22 +1,22 @@
 function value = fitness(cpushare, wm, model, params)
     
     workmix = getProbMix(wm);
-    nuser = getCurrentUsers(); % TODO obtain from Redis database
+    W = getCurrentUsers(); % TODO obtain from Redis database
 
-    rv = [nuser, nuser, nuser, nuser];
+    rv = [W, W, W, W];
     
     tempName = strcat('fittmp', getDateString());
     tempPath = strcat('./out/', tempName, '.', model.extension);
 
     [np2, st2] = calculateByCPUShare(model.st, cpushare);
 
-    updateModel(model.template_path, tempPath, 'nuser', nuser);
+    updateModel(model.template_path, tempPath, 'W', W);
     updateModel(tempPath, tempPath, 'wm', workmix);
     updateModel(tempPath, tempPath, 'rv', rv);
     updateModel(tempPath, tempPath, 'st', st2);
     updateModel(tempPath, tempPath, 'np', np2);
     
     %% Calculate the Theta
-    value = solveModel(tempName, model, params, cpushare, nuser);
+    value = solveModel(tempName, model, params, cpushare, W);
     value = -value;
 end
