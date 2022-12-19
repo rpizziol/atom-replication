edd52cc2dbbb7a6acf4f01b5a5476c36ea943e54
rpizziol <roberto.pizziol@gmail.com>
2022-12-19 21:13:30 +0100

    Update

--- GA/test.m
+++ GA/test.m
@@ -4,6 +4,8 @@ addpath('./res/');
 [model, params] = initializeSockShop();
 wm = 'b';
 
-a = fitness([1.5, 2.8, 2.6, 1.9], wm, model, params);
 
-disp(a);
+runExperiment(wm, model, params);
+%a = fitness([1.5, 2.8, 2.6, 1.9], wm, model, params);
+
+%disp(a);
