972efadeb230907c3e282c26781ad3e79563f693
rpizziol <roberto.pizziol@gmail.com>
2023-01-03 15:22:01 +0100

    Debug

--- GA/utility/getCurrentUsers.m
+++ GA/utility/getCurrentUsers.m
@@ -1,7 +1,8 @@
 function nusers = getCurrentUsers()
 %GETCURRENTUSERS Obtain the number of the users from the database.
 %   Temporarely mock function that returns a random number of users.
-    w = [600, 1200, 1800, 2400, 2800, 3200];
-    nusers = w(randi(size(w,2)));
+    %w = [600, 1200, 1800, 2400, 2800, 3200];
+    %nusers = w(randi(size(w,2)));
+    nusers = 1000;
 end
 
