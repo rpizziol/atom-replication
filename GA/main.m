e2ef6dd747f5314ab6d4108082f1f3a81e6f4af1
rpizziol <roberto.pizziol@gmail.com>
2023-01-03 11:39:26 +0100

    function main

--- GA/main.m
+++ GA/main.m
@@ -1,11 +1,15 @@
-% Functions relative to the web application used
-addpath('./apps/');
-% Utility functions
-addpath('./utility/');
-% xml and lqn templates defining the models
-addpath('./res/');
+function main(code)% Functions relative to the web application used
+    addpath('./apps/');
+    % Utility functions
+    addpath('./utility/');
+    % xml and lqn templates defining the models
+    addpath('./res/');
+    
+    if (code == 1)
+        [model, params] = initializeSockShop('b');
+    elseif (code == 2)
+        [model, params] = initializeAcmeAir();
+    end
 
-%[model, params] = initializeSockShop('b');
-[model, params] = initializeAcmeAir();
-
-runExperiment(model, params);
+    runExperiment(model, params);
+end
