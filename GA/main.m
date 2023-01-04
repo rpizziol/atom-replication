45ceeeee5282cfc3794e206b9e4e93d556d93bdc
rpizziol <roberto.pizziol@gmail.com>
2023-01-04 12:09:04 +0100

    Redis + startup application

--- GA/main.m
+++ GA/main.m
@@ -4,12 +4,26 @@ function main(code)% Functions relative to the web application used
     addpath('./utility/');
     % xml and lqn templates defining the models
     addpath('./res/');
+    % Redis
+    addpath('/root/git/MatlabRedis');       
     
     if (code == 1)
         [model, params] = initializeSockShop('b');
+        runExperiment(model, params);
     elseif (code == 2)
-        [model, params] = initializeAcmeAir();
-    end
+        % Initialize application
+        server = "localhost";
+        port = 27017;
+        dbname = "sys";
+        conn = mongoc(server, port, dbname);
+        if(~isopen(conn))
+            error("error whilt connecting to mongodb")
+        end
 
-    runExperiment(model, params);
+        initSim(conn);
+        startSystem(conn);
+
+        [model, params] = initializeAcmeAir();
+        runExperiment(model, params);
+    end
 end
--- GA/test/lqnsTest.m
+++ GA/test/lqnsTest.m
@@ -12,3 +12,11 @@ updateModel(tempfile, tempfile, 'W', [1000]);
 updateModel(tempfile, tempfile, 'np', np);
 
 [status, ~] = system("lqns -x " + tempfile, '-echo');
+
+% Calculate total time Acme Air
+
+stAcme = [0.2738, 0.151, 0.0812, 0.1505, 0.2013, 0.0403, 0.1008, ...
+        0.0876, 0.0908, 0.0504];
+reps = [1, 1, 1, 2, 1, 4, 1, 2, 1, 4];
+
+totTime = sum(stAcme .* reps); % = 1.7379
