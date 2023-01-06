61594c76b645c7f2f698c9e2d165997002ea4508
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-06 17:14:53 +0100

    minor

--- GA/printState.m
+++ GA/printState.m
@@ -42,7 +42,7 @@ function state = printState(options, state, flag,model)
 
         %qui l'attuazione del nuovo cpushare del numero di server
         disp(model.ms);
-        for i=1:length(model.ms,1)
+        for i=1:length(model.ms)
             updateShare(model.ms(i),bestIndividual(i),model.redisConn)
         end
 
