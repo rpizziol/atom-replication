6ca906fb93764b10188e0c95484db908041f01d8
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-13 09:51:09 +0100

    minor

--- GA/utility/parseOut/parseRep.m
+++ GA/utility/parseOut/parseRep.m
@@ -8,15 +8,19 @@ function [traceS] = parseRep(resPath)
         end
     end
     
-    traceS=zeros(9,round(maxT)+1,size(file,1));
-
+    traceS=zeros(10,round(maxT)+1,size(file,1));
+    
     
     for i=1:size(file,1)
         repData=load(sprintf("%s/%s",file(i).folder,file(i).name));
-
+        
         tsin = timeseries(repData.bestIndividuals,repData.bestTimeStamps-repData.bestTimeStamps(1));
         tsout = resample(tsin, linspace(0, round(maxT), ceil(round(maxT))+1), 'zoh');
-        traceS(:, :, i) = tsout.Data';
+        traceS(1:9, :, i) = tsout.Data';
+
+        tsin = timeseries(repData.bestThroughputs,repData.bestTimeStamps-repData.bestTimeStamps(1));
+        tsout = resample(tsin, linspace(0, round(maxT), ceil(round(maxT))+1), 'zoh');
+        traceS(10, :, i) = tsout.Data';
     end
 end
 
