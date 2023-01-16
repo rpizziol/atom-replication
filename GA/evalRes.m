2df228794546dcc48db68634200c051a03fed65e
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-16 20:15:15 +0100

    minor

--- GA/evalRes.m
+++ GA/evalRes.m
@@ -18,12 +18,14 @@ gaunT=zeros(1,size(gaun.data,1));
 gacsT=zeros(1,size(gaun.data,1));
 mudataT=zeros(1,size(gaun.data,1));
 
+newIDX=[3     4    10    11     6     5     7     9     8];
+
 [model, params] = initializeAcmeAir();
 for i=1:size(gaun.data,1)    
     currNuser=gaun.data(i,2);
     
     disp(i);
-    gaunT(1,i)=getThroughputByCPUShare(fillmissing(gaun.data(i,3:end),"constant",1), model);
-    gacsT(1,i)=getThroughputByCPUShare(fillmissing(gacs.dataga_cstr(i,3:end),"constant",1), model);
-    mudataT(1,i)=getThroughputByCPUShare(fillmissing(mudata.data_mu(i,3:end),"constant",1), model);
+    gaunT(1,i)=getThroughputByCPUShare(fillmissing(gaun.data(i,newIDX),"constant",1), model);
+    gacsT(1,i)=getThroughputByCPUShare(fillmissing(gacs.dataga_cstr(i,newIDX),"constant",1), model);
+    mudataT(1,i)=getThroughputByCPUShare(fillmissing(mudata.data_mu(i,newIDX),"constant",1), model);
 end
