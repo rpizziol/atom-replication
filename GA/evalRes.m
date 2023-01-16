7533c2784b2c3f8ea4c5c1d2e761d000f694516f
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-16 18:25:25 +0100

    minor

--- GA/evalRes.m
+++ GA/evalRes.m
@@ -16,13 +16,13 @@ addpath('./res');
 
 gaunT=zeros(1,size(gaun.data,1));
 gacsT=zeros(1,size(gaun.data,1));
-mudata=zeros(1,size(gaun.data,1));
+mudataT=zeros(1,size(gaun.data,1));
 
 [model, params] = initializeAcmeAir();
 for i=size(gaun.data,1)    
     currNuser=gaun.data(i,2);
 
     gaunT(1,i)=getThroughputByCPUShare(gaun.data(i,3:end), model);
-    gacsT(1,i)=getThroughputByCPUShare(gacs.data(i,3:end), model);
-    mudata(1,i)=getThroughputByCPUShare(mudata.data(i,3:end), model);
+    gacsT(1,i)=getThroughputByCPUShare(gacs.dataga_cstr(i,3:end), model);
+    mudataT(1,i)=getThroughputByCPUShare(mudata.data_mu(i,3:end), model);
 end
