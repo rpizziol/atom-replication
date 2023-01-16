db61571b95f8bbb1fb290bea194d3c60da1d1088
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-16 10:46:31 +0100

    added nlconstraint

--- GA/utility/nlcon.m
+++ GA/utility/nlcon.m
@@ -1,12 +1,6 @@
 function [c, ceq] = nlcon(x,model)
-% model.ms=["MSauth","MSvalidateid","MSviewprofile","MSupdateprofile"...
-%     ,"MSupdateMiles","MSbookflights","MScancelbooking"...
-%     ,"MSqueryflights","MSgetrewardmiles"];
-
-RTmax=[1.8372;0.2400;0.0848;0.1536;0.2045;0.0446;0.3392;0.1951;0.0945;0.0527];
 RT = getRTByCPUShare(x, model);
-
-c = RT-RTmax;
+c = RT-model.RTmax;
 % No nonlinear equality constraints:
 ceq = [];
 end
