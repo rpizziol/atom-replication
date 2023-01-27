396a3b2076371f681cfeeeb9e391f337b097e28b
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-27 12:19:15 +0100

    minor

--- LQN-CRN/controller/acmeAir/acmeCtrl.jl
+++ LQN-CRN/controller/acmeAir/acmeCtrl.jl
@@ -249,7 +249,7 @@ subscribe(channels...; stop_fn=stop_fn, client=subscriber) do msg
 	#qui la logica di attuazione
 	#multi()
 	for m=1:length(NC)
-		set(@sprintf("%s_hw",MS[m]),value(NC[m+1])+0.05*Ik;client=redis_cli)
+		set(@sprintf("%s_hw",MS[m]),max(value(NC[m+1])+0.05*Ik,0.5);client=redis_cli)
 	end
 	#results = exec()
 end
