faa26ba574d7bcadf0da0cf569778591e21e6feb
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-02-17 10:45:30 +0100

    data

--- LQN-CRN/controller/acmeAir/acmeCtrl.jl
+++ LQN-CRN/controller/acmeAir/acmeCtrl.jl
@@ -232,7 +232,7 @@ subscribe(channels...; stop_fn=stop_fn, client=subscriber) do msg
 	w=parse(Float64,get("users";client=redis_cli))
 	set_value(C,w)
 
-    @objective(model,Max,0.5*(T[1])*1.0/(0.77*w)-0.5*(sum(NC)+0*sum(NT))/(maxNC*9+0*maxNT*9))
+    @objective(model,Max,0.5*(T[1])*1.0/(0.75*w)-0.5*(sum(NC)+0*sum(NT))/(maxNC*9+0*maxNT*9))
     global stimes=@elapsed JuMP.optimize!(model)
     global status=termination_status(model)
     if(status!=MOI.LOCALLY_SOLVED && status!=MOI.ALMOST_LOCALLY_SOLVED)
@@ -240,9 +240,8 @@ subscribe(channels...; stop_fn=stop_fn, client=subscriber) do msg
     end
 
 	Tmk=getTr(mongoClient,10,"MSauth")
-	println(Tmk," ",0.77*w," ",Ik)
 	if(typeof(Tmk)!=Nothing)
-		global Ik=Ik+((0.77*w)-Tmk)
+		global Ik=Ik+((0.75*w)-Tmk)
 	else
 		println("error")
 		global Ik=0
@@ -251,8 +250,7 @@ subscribe(channels...; stop_fn=stop_fn, client=subscriber) do msg
 	#qui la logica di attuazione
 	#multi()
 	for m=1:length(NC)
-		#println(@sprintf("%s_hw",MS[m]),value(NC[m+1])+0.1*Ik)
-		set(@sprintf("%s_hw",MS[m]),@sprintf("%.3f",value(NC[m+1])+0.001*Ik);client=redis_cli)
+		set(@sprintf("%s_hw",MS[m]),@sprintf("%.3f",value(NC[m+1])+0.0005*Ik);client=redis_cli)
 	end
 	#results = exec()
 end
